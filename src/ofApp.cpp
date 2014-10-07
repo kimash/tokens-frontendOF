af9fc68b0e85ba34ecd5438d3828f167bfe831b8
Kim Ash <ash.kimberly.b@gmail.com>
2014-10-07 15:27:55 -0400

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,71 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    std::string url = "http://tokensgame-web-ui.herokuapp.com/data/rounds";
+    
+    // Now parse the JSON
+    bool parsingSuccessful = json.open(url);
+    
+    if (parsingSuccessful)
+    {
+        ofLogNotice("ofApp::setup") << json.getRawString(true);
+    } else {
+        ofLogNotice("ofApp::setup") << "Failed to parse JSON.";
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
